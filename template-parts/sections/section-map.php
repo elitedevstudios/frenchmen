db7faf57914cb78f1f1eb4f6571794532e1510da
Christopher Smikle <christophersmikle@Christophers-MacBook-Pro.local>
2026-02-20 18:35:15 -0500

    fix: Update Google Maps embed for Pearly Beach, Jamaica

--- template-parts/sections/section-map.php
+++ template-parts/sections/section-map.php
@@ -10,14 +10,14 @@ defined( 'ABSPATH' ) || exit;
 <section class="fm-map" aria-label="<?php esc_attr_e( 'Map to Pearly Beach Ocho Rios', 'frenchmen' ); ?>">
 	<div class="fm-map__container">
 		<iframe 
-			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3785.8234567890123!2d-77.1234567!3d18.4234567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sPearly%20Beach%2C%20Ocho%20Rios!5e0!3m2!1sen!2sjm!4v1234567890123"
+			src="https://www.google.com/maps?q=Pearly+Beach,+Ocho+Rios,+Jamaica&output=embed"
 			width="100%" 
 			height="450" 
 			style="border:0;" 
 			allowfullscreen="" 
 			loading="lazy" 
 			referrerpolicy="no-referrer-when-downgrade"
-			title="Map to Pearly Beach, Ocho Rios"
+			title="Map to Pearly Beach, Jamaica"
 		></iframe>
 	</div>
 </section>
